c48d2db1672026b5b33ddce6205fad049711e1d8
Dhins <dhins333@gmail.com>
2020-02-13 15:48:17 +0530

    Video feed

--- add.py
+++ add.py
@@ -1,6 +1,7 @@
 import dlib
 import numpy as np
 import imageio
+import cv2
 
 face_detector = dlib.get_frontal_face_detector()
 pose_predictor_68_point = dlib.shape_predictor('./models/shape_predictor_68_face_landmarks.dat')
@@ -20,9 +21,18 @@ def whirldata_face_encodings(face_image,num_jitters=1):
     predictor = pose_predictor(face_image, face_location) 
     return np.array(face_encoder.compute_face_descriptor(face_image, predictor, num_jitters))
 
-known_image = imageio.imread("one.jpeg")
-enc = whirldata_face_encodings(known_image)
+video_capture = cv2.VideoCapture(0)
+while True:
+    ret, frame = video_capture.read()
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('c'):
+         break	
+small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+rgb_small_frame = small_frame[:, :, ::-1]
+enc = whirldata_face_encodings(rgb_small_frame)
 if(len(enc)!=0):
     print("Enter the name for the student")
     name = input()
     np.savetxt("./dataset/"+name+".csv",enc,delimiter=",")
+video_capture.release()
+cv2.destroyAllWindows()
